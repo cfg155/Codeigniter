ec6cd90e3f24aae24c79011c1bccc094b546f123
cfg155 <leonardolouis2@gmail.com>
2021-03-23 11:45:02 +0700

    how to create layout and template

---
+++ byWebProgrammingUNPAS/appstarter/app/Views/pages/about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>This is About Page</h1>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, optio cum officia voluptate, vel earum, assumenda fugiat voluptates vero magnam delectus id unde. Repudiandae esse perspiciatis delectus autem, mollitia hic eveniet totam qui dolor architecto blanditiis illo rerum quia. Dolor eum saepe rerum officiis hic facilis asperiores, repellat modi neque.
+</body>
+
+</html>
